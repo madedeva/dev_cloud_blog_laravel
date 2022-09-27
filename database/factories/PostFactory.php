ef42b57719be891330aed4cbfd77a28a7514de6b
madedeva <deva.kerti@undiksha.ac.id>
2022-09-27 14:21:24 +0800

    redesign layout

--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -24,7 +24,6 @@ class PostFactory extends Factory
             'title' => $this->faker->sentence(mt_rand(4,8)),
             'slug' => $this->faker->slug(),
             'excerpt' => $this->faker->paragraph(),
-            // 'body' => '<p>' .  implode('</p><p>', $this->faker->paragraphs(mt_rand(5,10))) . '</p>',
             'body' => collect($this->faker->paragraphs(mt_rand(5,10)))
                 ->map(fn($p) => "<p>$p</p>")
                 ->implode(''),
